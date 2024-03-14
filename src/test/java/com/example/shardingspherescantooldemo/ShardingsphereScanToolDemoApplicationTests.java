7eb7910ec2b1d05cdbf9684fd163dfdb8ca176ea
risk <399284508@qq.com>
2024-03-14 11:40:52 +0800

    init repository

---
+++ src/test/java/com/example/shardingspherescantooldemo/ShardingsphereScanToolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shardingspherescantooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereScanToolDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
