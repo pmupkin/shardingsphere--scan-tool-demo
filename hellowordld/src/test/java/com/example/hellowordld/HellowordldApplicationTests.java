3f98ee71f73561185aa7f2e20e9158136823071f
risk <399284508@qq.com>
2024-03-15 08:20:55 +0800

    init re

---
+++ hellowordld/src/test/java/com/example/hellowordld/HellowordldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowordld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
