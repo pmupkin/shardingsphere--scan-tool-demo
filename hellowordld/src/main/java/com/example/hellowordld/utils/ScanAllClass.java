10062e4ba330efeb8e3a8aa1b8780fd76e89b0a5
pmupkin <399284508@qq.com>
2024-03-21 00:08:08 +0800

    test github-action v0.1 -fix run java path -2

--- hellowordld/src/main/java/com/example/hellowordld/utils/ScanAllClass.java
+++ hellowordld/src/main/java/com/example/hellowordld/utils/ScanAllClass.java
@@ -13,9 +13,9 @@ public class ScanAllClass {
     //"target/test-classes/";
     public static void main(String[] args) {
        //String projectPath = args.length > 0 ? args[0] : "."; // 可以从命令行参数获取项目路径
-        File classesDir = new File(CLASSES_PATH);
+        File classesDir = new File(".");
         File testClassesDir = new File(TEST_CLASSES_PATH);
-        if (classesDir.exists() && classesDir.isDirectory() && testClassesDir.exists() && testClassesDir.isDirectory()) {
+        if (classesDir.exists() && classesDir.isDirectory()) { // && testClassesDir.exists() && testClassesDir.isDirectory()) {
             scanClasses(classesDir, testClassesDir);
         } else {
             System.out.println("提供的路径不是有效的目录");
@@ -32,9 +32,10 @@ public class ScanAllClass {
                     // 如果是文件，检查是否以.CLASS_FILE_EXTENSION结尾
                     if (file.getName().endsWith(CLASS_FILE_EXTENSION)) {
                         String className = classNameFromFile(file, CLASSES_PATH);
+                        System.out.println("className->"+className);
                         //String testClassName = testClassNameFromClassName(className) + TEST_CLASS_SUFFIX;
                         String testClassPath = TEST_CLASSES_PATH + className.replace('.', '/') + TEST_CLASS_SUFFIX +  CLASS_FILE_EXTENSION;
-                        //System.out.println("测试类路径:"+testClassPath);
+                        System.out.println("测试类路径:"+testClassPath);
                         File testClassFile = new File(testClassPath);
                         if (!testClassFile.exists()) {
                             System.out.println("没有找到对应的单元测试类: " + className);
@@ -54,7 +55,7 @@ public class ScanAllClass {
     }
     private static String classNameFromFile(File classFile, String pathPrefix) {
         String path = classFile.getAbsolutePath();
-        //System.out.println("path:"+path);
+        System.out.println("path:"+path);
         String className = path.substring(path.indexOf(pathPrefix) + pathPrefix.length()+1, path.indexOf(CLASS_FILE_EXTENSION));
         className = className.replace(File.separatorChar, '.');
         return className;
