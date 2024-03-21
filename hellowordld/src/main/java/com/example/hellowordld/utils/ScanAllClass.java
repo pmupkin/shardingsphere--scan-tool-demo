6f5b44c0ea580ca0a7aedf12edb4b2fc9a5918d6
pmupkin <399284508@qq.com>
2024-03-21 23:52:27 +0800

    test github-action v0.2 - publish issue

--- hellowordld/src/main/java/com/example/hellowordld/utils/ScanAllClass.java
+++ hellowordld/src/main/java/com/example/hellowordld/utils/ScanAllClass.java
@@ -10,13 +10,21 @@ public class ScanAllClass {
     private static final String TEST_CLASS_SUFFIX = "Tests";
     private static final String CLASSES_PATH = "./target/classes";
     private static final String TEST_CLASSES_PATH = "./target/test-classes/";
+
+    private static String resMd;
     //"target/test-classes/";
     public static void main(String[] args) {
        //String projectPath = args.length > 0 ? args[0] : "."; // 可以从命令行参数获取项目路径
+        resMd = "";
         File classesDir = new File(CLASSES_PATH);
         File testClassesDir = new File(TEST_CLASSES_PATH);
         if (classesDir.exists() && classesDir.isDirectory()) { // && testClassesDir.exists() && testClassesDir.isDirectory()) {
             scanClasses(classesDir, testClassesDir);
+            if(!resMd.isEmpty()){
+                resMd = "# Unit Test Coverage Report\n" + "The following classes are missing unit tests and should be addressed promptly: \n\n" + resMd
+                    + "\n\nPlease ensure that comprehensive test coverage is provided for these classes to maintain code quality and the stability of the project. Your contributions are greatly appreciated!\n";
+            }
+            System.out.println(resMd);
         } else {
             System.out.println("提供的路径不是有效的目录");
         }
@@ -32,13 +40,13 @@ public class ScanAllClass {
                     // 如果是文件，检查是否以.CLASS_FILE_EXTENSION结尾
                     if (file.getName().endsWith(CLASS_FILE_EXTENSION)) {
                         String className = classNameFromFile(file, CLASSES_PATH);
-                        System.out.println("className->"+className);
+                        //System.out.println("className->"+className);
                         //String testClassName = testClassNameFromClassName(className) + TEST_CLASS_SUFFIX;
                         String testClassPath = TEST_CLASSES_PATH + className.replace('.', '/') + TEST_CLASS_SUFFIX +  CLASS_FILE_EXTENSION;
-                        System.out.println("测试类路径:"+testClassPath);
+                        //System.out.println("测试类路径:"+testClassPath);
                         File testClassFile = new File(testClassPath);
                         if (!testClassFile.exists()) {
-                            System.out.println("没有找到对应的单元测试类: " + className);
+                            resMd += ("- " + className);
                         }
                     }
                 }
@@ -55,7 +63,7 @@ public class ScanAllClass {
     }
     private static String classNameFromFile(File classFile, String pathPrefix) {
         String path = classFile.getAbsolutePath();
-        System.out.println("path:"+path);
+        //System.out.println("path:"+path);
         String className = path.substring(path.indexOf(pathPrefix) + pathPrefix.length()+1, path.indexOf(CLASS_FILE_EXTENSION));
         className = className.replace(File.separatorChar, '.');
         return className;
